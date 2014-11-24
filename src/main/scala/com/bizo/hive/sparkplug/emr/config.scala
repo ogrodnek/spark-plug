fe81ca4f4561c6d003d5fca367cd15ad921e5e10
Larry Ogrodnek <larry@bizo.com>
2014-11-24 12:45:51 -0800

    clean up pricing a bit

--- src/main/scala/com/bizo/hive/sparkplug/emr/config.scala
+++ src/main/scala/com/bizo/hive/sparkplug/emr/config.scala
@@ -16,56 +16,69 @@ trait ClusterConfig {
 }
 
 class DefaultBidProvider extends BidProvider {
+  // bid standard on-demand prices:
+  // - http://aws.amazon.com/ec2/pricing/
+  // - http://aws.amazon.com/ec2/previous-generation/
   val bidPrices = Map(
-    // Standard 
-    "m1.small" -> 0.048,
-    "m1.medium" -> 0.096,
-    "m1.large" -> 0.193,
-    "m1.xlarge" -> 0.385,
+    // General Purpose - Previous Generation
+    "m1.small"  -> 0.044,
+    "m1.medium" -> 0.087,
+    "m1.large"  -> 0.175,
+    "m1.xlarge" -> 0.350,
+    
+    // Compute Optimized - Previous Generation
+    "c1.medium"   -> 0.130,
+    "c1.xlarge"   -> 0.520,
+    "cc2.8xlarge" -> 2.000,
+    "cc1.4xlarge" -> 1.300,
+    
+    // GPU Instances - Previous Generation
+    "cg1.4xlarge" -> 2.100,
 
-    // Second Generation
-    "m3.medium" -> 0.077,
-    "m3.large" -> 0.154,
-    "m3.xlarge" -> 0.308,
-    "m3.2xlarge" -> 0.616,
-
-    // Micro
-    "t1.micro" -> 0.02,
-
-    // High-Memory
-    "r3.large" -> 0.193,
-    "r3.xlarge" -> 0.385,
-    "r3.2xlarge" -> 0.77,
-    "r3.4xlarge" -> 1.54,
-    "r3.8xlarge" -> 2.8,
-    "m2.xlarge" -> 0.27,
-    "m2.2xlarge" -> 0.539,
-    "m2.4xlarge" -> 1.078,
-
-    // High-CPU
-    "c3.large" -> 0.116,
-    "c3.xlarge" -> 0.231,
-    "c3.2xlarge" -> 0.462,
-    "c3.4xlarge" -> 0.924,
-    "c3.8xlarge" -> 1.68,
-    "c1.medium" -> 0.143,
-    "c1.xlarge" -> 0.572,
-
-    // Cluster Compute
-    "cc1.4xlarge" -> 1.3,
-    "cc2.8xlarge" -> 2.0,
-
-    // High-Memory Cluster
-    "cr1.8xlarge" -> 3.5,
-
-    // Cluster GPU
-    "cg1.4xlarge" -> 2.1,
-
-    // High-I/O
-    "hi1.4xlarge" -> 3.1,
-
-    // High-Storage
-    "hs1.8xlarge" -> 4.6
+    // Memory Optimized - Previous Generation
+    "m2.xlarge"   -> 0.245,
+    "m2.2xlarge"  -> 0.490,
+    "m2.4xlarge"  -> 0.980,
+    "cr1.8xlarge" -> 3.500,
+    
+    // Storage Optimized - Previous Generation
+    "hi1.4xlarge" -> 3.100,
+    
+    // Micro Instances - Previous Generation
+    "t1.micro" -> 0.020,    
+    
+    // General Purpose - Current Generation
+    "t2.micro"    -> 0.013,
+    "t2.small"    -> 0.026,
+    "t2.medium"   -> 0.052,
+    "m3.medium"   -> 0.070,
+    "m3.large"    -> 0.140,
+    "m3.xlarge"   -> 0.280,
+    "m3.2xlarge"  -> 0.560,
+    
+    // Compute Optimized - Current Generation
+    "c3.large"   -> 0.105,
+    "c3.xlarge"  -> 0.210,
+    "c3.2xlarge" -> 0.420,
+    "c3.4xlarge" -> 0.840,
+    "c3.8xlarge" -> 1.680,
+    
+    // GPU Instances - Current Generation
+    "g2.2xlarge" -> 0.650,
+    
+    // Memory Optimized - Current Generation
+    "r3.large"   -> 0.175,
+    "r3.xlarge"  -> 0.350,
+    "r3.2xlarge" -> 0.700,
+    "r3.4xlarge" -> 1.400,
+    "r3.8xlarge" -> 2.800,
+    
+    // Storage Optimized - Current Generation
+    "i2.xlarge"   -> 0.853,
+    "i2.2xlarge"  -> 1.705,
+    "i2.4xlarge"  -> 3.410,
+    "i2.8xlarge"  -> 6.820,
+    "hs1.8xlarge" -> 4.600
   )
 
   override def bid(size: String): Double = bidPrices(size)
